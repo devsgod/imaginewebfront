9026c97dae8a18095b85b586139e124f52eb216b
devsgod <developer9517@yandex.com>
2020-06-03 06:00:41 +0300

    Imagine Front 63_2

--- components/Checkout/CheckoutBody.js
+++ components/Checkout/CheckoutBody.js
@@ -3,8 +3,8 @@ import React, {useEffect, useState} from 'react';
 // import useForm from './userForm';
 import {connect} from "react-redux";
 import axios from "axios";
-// import config from "../../config/config.json";
-import config from "../../config/config_local.json";
+import config from "../../config/config.json";
+// import config from "../../config/config_local.json";
 
 import * as cartAction from "../../store/actions/cartActions";
 import CustomPaypal from "../Payment/customPayment";
--- pages/success-payment.js
+++ pages/success-payment.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 import Navbar from '../components/Layouts/Navbar';
 import Footer from '../components/Layouts/Footer';
 import axios from "axios";
-// import config from "../config/config.json";
-import config from "../config/config_local.json";
+import config from "../config/config.json";
+// import config from "../config/config_local.json";
 
 class TeamOne extends Component {
 
