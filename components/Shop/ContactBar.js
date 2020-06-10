4c0bd23dfd1bf366b554ac36addf8c19fa4c3d5b
devsgod <developer9517@yandex.com>
2020-06-10 20:28:04 +0300

    Imagine front 610_3

--- components/Shop/ContactBar.js
+++ components/Shop/ContactBar.js
@@ -8,60 +8,52 @@ const ContactBar = (props) => {
             </div>
             <div className='contact-body'>
                 <div className='row'>
-                    <div className='col-3'><img style={{marginTop:"20px", padding:"5px"}} src={require("../../images/shop/sidebar_svg/1. Imagine.png")}/></div>
+                    <div className='col-3'><img className="shop-nav-subimg" src={require("../../images/shop/sidebar_svg/1. Imagine.png")}/></div>
                     <div className='col-9'>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtitle">
                             IMAGINE SOFTWARE
                         </div>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtext">
                             Pre-installed and configured
                         </div>
                     </div>
-                    <div style={{height: '10px', width : '100%', borderBottom : '1px solid #ffc800',
-                        marginLeft : '40px', marginRight : '40px'
-                    }}></div>
+                    <div className="shop-nav-subline"></div>
                 </div>
                 <div className='row'>
-                    <div className='col-3'><img style={{marginTop:"20px", padding:"5px"}} src={require("../../images/shop/sidebar_svg/2. Payment.svg")}/></div>
+                    <div className='col-3'><img className="shop-nav-subimg" src={require("../../images/shop/sidebar_svg/2. Payment.svg")}/></div>
                     <div className='col-9'>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtitle">
                             PAYMENT PROTECTION
                         </div>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtext">
                             South Africa and international
                         </div>
                     </div>
-                    <div style={{height: '10px', width : '100%', borderBottom : '1px solid #ffc800',
-                        marginLeft : '40px', marginRight : '40px'
-                    }}></div>
+                    <div className="shop-nav-subline"></div>
                 </div>
                 <div className='row'>
-                    <div className='col-3'><img style={{marginTop:"20px", padding:"5px"}} src={require("../../images/shop/sidebar_svg/3. Delivery.svg")}/></div>
+                    <div className='col-3'><img className="shop-nav-subimg" src={require("../../images/shop/sidebar_svg/3. Delivery.svg")}/></div>
                     <div className='col-9'>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtitle">
                             DELIVERY AVAILABLE
                         </div>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtext">
                             Local or international delivery
                         </div>
                     </div>
-                    <div style={{height: '10px', width : '100%', borderBottom : '1px solid #ffc800',
-                        marginLeft : '40px', marginRight : '40px'
-                    }}></div>
+                    <div className="shop-nav-subline"></div>
                 </div>
                 <div className='row'>
-                    <div className='col-3'><img style={{marginTop:"20px", padding:"5px"}} src={require("../../images/shop/sidebar_svg/4. Warranty.svg")}/></div>
+                    <div className='col-3'><img className="shop-nav-subimg" src={require("../../images/shop/sidebar_svg/4. Warranty.svg")}/></div>
                     <div className='col-9'>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtitle">
                             WARRANTY
                         </div>
-                        <div style={{paddingTop : '20px'}}>
+                        <div className="shop-nav-subtext">
                             2~3 year Manufacture warranty
                         </div>
                     </div>
-                    <div style={{height: '10px', width : '100%',
-                        marginLeft : '40px', marginRight : '40px'
-                    }}></div>
+                    <div className="shop-nav-subline"></div>
                 </div>
             </div>
         </div>
